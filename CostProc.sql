65f0e02b6fd71caaf62a70ef3e201d12543e76f2
Brandon Oh <brandonoh2938@gmail.com>
2022-03-09 19:51:30 -0500

    Animal info and Cost Procedure.

---
+++ CostProc.sql
@@ -0,0 +1,16 @@
+USE db_zoo
+GO
+
+DECLARE @totalHab MONEY;
+DECLARE @totalNut MONEY;
+DECLARE @results MONEY;
+
+SET @totalHab = (SELECT SUM(habitat_cost) FROM tbl_habitat);
+SET @totalNut = (SELECT SUM(nutrition_cost) FROM tbl_nutrition);
+SET @results = (@totalHab + @totalNut);
+
+PRINT (
+	CONVERT(VARCHAR(50), @totalHab) + CHAR(9) + ' - The Total Habitat Cost' + CHAR(13) +
+	CONVERT(VARCHAR(50), @totalNut) + CHAR(9) + CHAR(9) + ' - The Total Nutrition Cost' + CHAR(13) + '------------' + CHAR(13) +
+	CONVERT(VARCHAR(50), @results)
+);
